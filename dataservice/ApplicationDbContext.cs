b78714164cce60c1c1926810e4b873609afd1f1a
lifeofperfect <perfect.igbadumhe@gmail.com>
2021-08-01 16:55:04 +0100

    creating database with entity framework

---
+++ dataservice/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ModelService;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataService
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>().HasData(
+                new {id=1, Name="Administrator", NormalizedName="ADMINISTRATOR", RoleName="Administrator", Handle="administartor", RoleIcon="/uploads/roles/icons/default/role.png", IsActive=true},
+                new {id=2, Name="Customer", NormalizedName="CUSTOMER", RoleName="customer", Handle="customer", RoleIcon="/uploads/roles/icons/default/role.png", IsActive=true}
+                );
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<AddressModel> Addresses { get; set; }
+    }
+}
