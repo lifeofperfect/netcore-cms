d0c3b95b3e022c430c617f831dc93724f14828b9
lifeofperfect <perfect.igbadumhe@gmail.com>
2021-08-01 16:00:36 +0100

    added serilog service

--- cms/Program.cs
+++ cms/Program.cs
@@ -1,9 +1,15 @@
+using LoggingService;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,13 +19,45 @@ namespace cms
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            
+            var host = CreateHostBuilder(args).Build();
+
+            using(var scope = host.Services.CreateScope())
+            {
+                try
+                {
+                    int zero = 0;
+                    int result = 100 / zero;
+                }
+                catch (DivideByZeroException ex)
+                {
+
+                    Log.Error("An Error occured while seeding the database {Error} {Stacktree} {InnerException} {Source}", ex.Message, ex.StackTrace, ex.Source);
+                }
+            }
+
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseSerilog((hostingContext, loggerConfiguration)=> loggerConfiguration
+                        .Enrich.FromLogContext()
+                        .Enrich.WithProperty("Application", "CMS")
+                        .Enrich.WithProperty("MachineName", Environment.MachineName)
+                        .Enrich.WithProperty("CurrentManagedThreadId", Environment.CurrentManagedThreadId)
+                        .Enrich.WithProperty("OSVersion", Environment.OSVersion)
+                        .Enrich.WithProperty("Version", Environment.Version)
+                        .Enrich.WithProperty("UserName", Environment.UserName)
+                        .Enrich.WithProperty("ProcessId", Process.GetCurrentProcess().Id)
+                        .Enrich.WithProperty("ProcessName", Process.GetCurrentProcess().ProcessName)
+                        .WriteTo.Console(theme: AnsiConsoleTheme.Code)
+                        .WriteTo.File(formatter: new CustomTextFormatter(), path: Path.Combine(hostingContext.HostingEnvironment.ContentRootPath + $"{Path.DirectorySeparatorChar}Logs{Path.DirectorySeparatorChar}", $"load_error_{DateTime.Now:yyyyMMdd}.txt"))
+                        .ReadFrom.Configuration(hostingContext.Configuration)
+                    );
                     webBuilder.UseStartup<Startup>();
                 });
     }
---
+++ loggingService/CustomTextFormatter.cs
@@ -0,0 +1,35 @@
+﻿using Serilog.Events;
+using Serilog.Formatting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LoggingService
+{
+    public class CustomTextFormatter : ITextFormatter
+    {
+        public void Format(LogEvent logEvent, TextWriter output)
+        {
+            output.WriteLine("-----------------------------------------------------------------------------------------------");
+            output.WriteLine($"Timestamp - {logEvent.Timestamp} | Level - {logEvent.Level} |");
+            output.WriteLine("-----------------------------------------------------------------------------------------------");
+            foreach(var item in logEvent.Properties)
+            {
+                output.WriteLine(item.Key + " : " + item.Value);
+            }
+
+            if(logEvent.Exception != null)
+            {
+                output.WriteLine("------------------------------Exception Details---------------------------------------------");
+                output.WriteLine("Exception - {0}", logEvent.Exception);
+                output.WriteLine("StackTrace - {0}", logEvent.Exception.StackTrace);
+                output.WriteLine("Message - {0}", logEvent.Exception.Message);
+                output.WriteLine("Source - {0}", logEvent.Exception.Source);
+                output.WriteLine("InnerException - {0}", logEvent.Exception.InnerException);
+            }
+
+            output.WriteLine("-------------------------------------------------------------------------------------------------");
+        }
+    }
+}
