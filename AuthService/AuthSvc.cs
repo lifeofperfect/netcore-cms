d85c940a2efabc19c98e3df21e6bc845371ad804
lifeofperfect <perfect.igbadumhe@gmail.com>
2021-08-08 21:21:52 +0100

    authorization completed

--- AuthService/AuthSvc.cs
+++ AuthService/AuthSvc.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.DependencyInjection;
 using ModelService;
 using Serilog;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Net;
@@ -25,7 +27,7 @@ namespace AuthService
         private readonly AppSettings _appSettings;
         private readonly ApplicationDbContext _db;
         private readonly IServiceProvider _provider;
-        private readonly DataProtectionKey _dataProtectionKeys;
+        private readonly DataProtectionKeys _dataProtectionKeys;
         private readonly ICookieService _cookieService;
         private readonly IActivityService _activityService;
         private IDataProtector _protector;
@@ -35,7 +37,7 @@ namespace AuthService
         private string unProtectedToken;
         private ClaimsPrincipal validateToken;
 
-        public AuthSvc(UserManager<ApplicationUser> userManager, IOptions<AppSettings> appSettings, IOptions<DataProtectionKey> dataProtectionKeys,
+        public AuthSvc(UserManager<ApplicationUser> userManager, IOptions<AppSettings> appSettings, IOptions<DataProtectionKeys> dataProtectionKeys,
             ApplicationDbContext db, IServiceProvider provider, IActivityService activityService, ICookieService cookieService)
         {
             _userManager = userManager;
@@ -95,7 +97,101 @@ namespace AuthService
 
         private async Task<TokenResponseModel> GenerateNewToken(ApplicationUser user, object model)
         {
-            
+            //create a key to encrypt the jwt
+            var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_appSettings.Secret));
+
+            //Get user role => check if user is admin
+            var roles = await _userManager.GetRolesAsync(user);
+
+            //createing jwt token
+            var tokenHander = new JwtSecurityTokenHandler();
+
+            //creating jwt token  and description for the token
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]{
+                    new Claim(JwtRegisteredClaimNames.Sub, user.UserName), //sub -identifies principal that issued the jwt
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), //Jti -unique identifier for the token
+                    new Claim(ClaimTypes.NameIdentifier, user.Id), //unique identifier for the user
+                    new Claim(ClaimTypes.Role, roles.FirstOrDefault()), // role of the user
+                    new Claim("LoggedOn", DateTime.Now.ToString(CultureInfo.InvariantCulture)) //time when created
+                }),
+                SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature),
+                Issuer = _appSettings.Site, //Issuer  -identifies principal that issues the jwt
+                Audience = _appSettings.Audience, //audience identifies the recipient that the jwt is intended for
+                Expires = (string.Equals(roles.FirstOrDefault(), "Admistrator", StringComparison.CurrentCultureIgnoreCase)) ? DateTime.UtcNow.AddMinutes(60): DateTime.UtcNow.AddMinutes(Convert.ToDouble(_appSettings.ExpireTime))
+            };
+
+            //create the token create unique encryption key for token  --2nd layer protection
+            var encryptionKeyRt = Guid.NewGuid().ToString();
+            var encryptionKeyJwt = Guid.NewGuid().ToString();
+
+            //Get the data protection service instance
+            var protectorProvider = _provider.GetService<IDataProtectionProvider>();
+
+            //create a protector instance
+            var protectorJwt = protectorProvider.CreateProtector(encryptionKeyJwt);
+
+            //generate token and protect the user token
+            var token = tokenHander.CreateToken(tokenDescriptor);
+            //ENCRYPT TOKEN
+            var encryptedToken = protectorJwt.Protect(tokenHander.WriteToken(token));
+
+            //create and update token table STORE THE token encryption key
+            TokenModel newRtoken = new TokenModel();
+
+            //create refresh token instance
+            newRtoken = CreateRefreshToken(_appSettings.ClientId, user.Id, Convert.ToInt32(_appSettings.RtExpireTime));
+
+            //assign the jwt encryption key
+            newRtoken.EncryptionKeyJwt = encryptionKeyJwt;
+
+            //assign rt encyption key
+            newRtoken.EncryptionKeyRt = encryptionKeyRt;
+
+            //addd refresh token with encryption key for jwt to db
+            try
+            {
+                //first we need to check if the user has already logged in  and as tokens in DB
+                var rt = _db.Tokens.FirstOrDefault(t => t.UserId == user.Id);
+
+                if(rt != null)
+                {
+                    //invalidate the old refresh token by deleting it
+                    _db.Tokens.Remove(rt);
+
+                    //add the new refresh tokens
+                    _db.Tokens.Add(newRtoken);
+                }
+                else
+                {
+                    await _db.Tokens.AddAsync(newRtoken);
+                }
+                //persist changes in the DB
+                await _db.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                Log.Error("An Error occured while seeding the database {Error} {Stacktree} {InnerException} {Source}", ex.Message, ex.StackTrace, ex.Source);
+            }
+
+            // Return response containing encrypted token
+            var protectorRt = protectorProvider.CreateProtector(encryptionKeyRt);
+            var layerOneProtector = protectorProvider.CreateProtector(_dataProtectionKeys.ApplicationUserKey);
+
+            var encAuthToken = new TokenResponseModel
+            {
+                Token = layerOneProtector.Protect(encryptedToken),
+                Expiration = token.ValidTo,
+                RefreshToken = protectorRt.Protect(newRtoken.Value),
+                Role = roles.FirstOrDefault(),
+                Username = user.UserName,
+                UserId = layerOneProtector.Protect(user.Id),
+                ResponseInfo = CreateResponse("Auth Token Created", HttpStatusCode.OK)
+            };
+
+            return encAuthToken;
         }
 
         //Private method
@@ -123,6 +219,20 @@ namespace AuthService
                 StatusCode = statusCode
             };
             return responseStatusInfo;
+        }
+
+        private static TokenModel CreateRefreshToken(string clientId, string userId, int expiryTime)
+        {
+            return new TokenModel()
+            {
+                ClientId = clientId,
+                UserId = userId,
+                Value = Guid.NewGuid().ToString("N"),
+                CreatedDate = DateTime.UtcNow,
+                ExpiryTime = DateTime.UtcNow.AddMinutes(expiryTime),
+                EncryptionKeyRt = "",
+                EncryptionKeyJwt = ""
+            };
         }
     }
 }
